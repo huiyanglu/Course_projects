aff5b4b1be19a46f101b9739eb23a142f46c4009
huiyang lu <luchocoice@gmail.com>
2019-03-26 22:33:11 +0800

    Update btc_close_2017.py

--- Chapter16_2_json/btc_close_2017.py
+++ Chapter16_2_json/btc_close_2017.py
@@ -17,6 +17,8 @@ import requests
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 import pygal
+import math
+from itertools import groupby
 
 """
 json_url = 'https://raw.githubusercontent.com/muxuezi/btc/master/btc_close_2017.json'
@@ -54,10 +56,33 @@ for btc_dict in btc_data:
     close.append(int(float(btc_dict['close'])))
     #print("{} is month {} week {}, {}, the close price is {} RMB".format(date,month,week,weekday,close))
 
-line_chart = pygal.Line(x_label_rotation=20,show_minor_x_labels=False)
-line_chart.title = '收盘价￥'
-line_chart.x_labels=dates
-N=20
-line_chart.x_labels_major = dates[::N]
-line_chart.add('收盘价',close)
-line_chart.render_to_file('收盘价折线图（￥）.svg')
+def draw_line(x_data,y_data,title,y_legend):
+    xy_map = []
+    for x,y in groupby(sorted(zip(x_data,y_data)),key=lambda _:_[0]):
+        y_list = [v for _,v in y]
+        xy_map.append([x,sum(y_list)/len(y_list)])
+    x_unique,y_mean = [*zip(*xy_map)]
+    line_chart = pygal.Line()
+    line_chart.title = title
+    line_chart.x_labels=x_unique
+    #N=20
+    #line_chart.x_labels_major = dates[::N]
+    #close_log = [math.log10(_) for _ in close] #用对数变换将非线性的趋势消除
+    line_chart.add(y_legend,y_mean)
+    line_chart.render_to_file(title+'.svg')
+    return line_chart
+
+idx_month = dates.index('2017-12-01')
+line_chart_month = draw_line(months[:idx_month],close[:idx_month],'收盘价月日均值（￥）','月日均值')
+line_chart_month
+
+idx_week = dates.index('2017-12-11')
+line_chart_week = draw_line(weeks[1:idx_week],close[1:idx_week],'收盘价周日均值（￥）','周日均价')
+line_chart_week
+
+with open('收盘价Dashboard.html','w',encoding='utf8') as html_file:
+    html_file.write('<html><head><title>收盘价Dashboard</title><meta charset="utf-8"></head><body>\n')
+    for svg in ['收盘价月日均值（￥）.svg','收盘价周日均值（￥）.svg']:
+        html_file.write(
+            '    <object type="image/svg+xml" data="{0}" height=500></object>\n'.format(svg))  # 1
+    html_file.write('</body></html>')
