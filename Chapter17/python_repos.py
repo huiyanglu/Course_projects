8af43345f65a2ce81d07a8162fdc1aff6ce9793c
huiyang lu <luchocoice@gmail.com>
2019-03-28 00:45:56 +0800

    Update python_repos.py

--- Chapter17/python_repos.py
+++ Chapter17/python_repos.py
@@ -10,9 +10,11 @@ response_dict = r.json()
 #print(response_dict.keys())
 print('Total repositories:',response_dict['total_count'])
 
+# 仓库信息 获得了多少个仓库
 repo_dicts = response_dict['items']
 print('Repositories returned:',len(repo_dicts))
 
+# 研究第一个仓库
 repo_dict = repo_dicts[0]
 print('\nKeys:',len(repo_dict))
 
