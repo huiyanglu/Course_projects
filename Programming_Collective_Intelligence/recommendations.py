a7f3f3f2c2353f83e38ebf3133419139403efe86
huiyang lu <luchocoice@gmail.com>
2019-03-31 23:54:46 +0800

    Update recommendations.py

--- Programming_Collective_Intelligence/recommendations.py
+++ Programming_Collective_Intelligence/recommendations.py
@@ -32,7 +32,83 @@ def sim_distance(prefs,person1,person2):
 
     if len(si)==0:return 0
 
+    sum_of_squares = sum([pow(prefs[person1][item]-prefs[person2][item],2)
+                          for item in prefs[person1] if item in prefs[person2]])
+
+    return 1/(1+sqrt(sum_of_squares))
+
+print(sim_distance(critics,'Lisa Rose','Gene Seymour'))
 
 
+def sim_person(prefs,p1,p2):
+    si = {}
+    for item in prefs[p1]:
+        if item in prefs[p2]:
+            si[item]=1
 
-print(critics['Toby']['Snakes on a Plane'])
+    n = len(si)
+
+    #若两人无共同之处，则返回1
+    if n==0:return 1
+
+    #对两人的偏好分别求和
+    sum1 = sum(prefs[p1][it] for it in si)
+    sum2 = sum(prefs[p2][it] for it in si)
+
+    #求平方和
+    sum1Sq = sum([pow(prefs[p1][it],2) for it in si])
+    sum2Sq = sum([pow(prefs[p2][it],2) for it in si])
+
+    #求乘积之和
+    pSum = sum([prefs[p1][it]*prefs[p2][it] for it in si])
+
+    num = pSum - (sum1*sum2/n)
+    den = sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
+
+    if den==0: return 0
+
+    r = num/den
+
+    return r
+
+print(sim_person(critics,'Lisa Rose','Gene Seymour'))
+
+def topMatches(prefs,person,n=5,similarity=sim_person):
+    scores = [(similarity(prefs,person,other),other) for other in prefs if other!=person]
+    scores.sort()
+    scores.reverse()
+
+    return scores[0:n]
+
+print(topMatches(critics,'Toby',n=3))
+
+
+def getRecommendations(prefs, person, similarity=sim_person):
+    totals = {}
+    simSums = {}
+    for other in prefs:
+        # don't compare me to myself
+        if other == person: continue
+        sim = similarity(prefs, person, other)
+
+        # ignore scores of zero or lower
+        if sim <= 0: continue
+        for item in prefs[other]:
+
+            # only score movies I haven't seen yet
+            if item not in prefs[person] or prefs[person][item] == 0:
+                # Similarity * Score
+                totals.setdefault(item, 0)
+                totals[item] += prefs[other][item] * sim
+                # Sum of similarities
+                simSums.setdefault(item, 0)
+                simSums[item] += sim
+
+    # Create the normalized list
+    rankings = [(total / simSums[item], item) for item, total in totals.items()]
+
+    # Return the sorted list
+    rankings.sort()
+    rankings.reverse()
+    return rankings
+
