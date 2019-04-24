30400c9275c3cd24da3a0bd5151b9eee2aee1aec
huiyanglu <luchocoice@gmail.com>
2019-04-24 17:15:48 +0800

    Update main.py

--- shiyanlou_ershoufang_info/main.py
+++ shiyanlou_ershoufang_info/main.py
@@ -4,6 +4,9 @@
 
 import sys
 import csv
+import requests
+from bs4 import BeautifulSoup
+
 
 def get_city_dict():
     city_dict = {}
@@ -14,19 +17,61 @@ def get_city_dict():
             city_dict[city[0]] = city[1]
     return city_dict
 
-city_dict = get_city_dict()
-#print(city_dict)
+def get_district_dict(url):
+    district_dict = {}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'
+    }
 
-for city_name in city_dict.keys():
-    print(city_name,end=' ') #如何不换行
+    html = requests.get(url, headers=headers)
+    html.encoding = 'utf-8'
+    bsobj = BeautifulSoup(html.text, 'html.parser')
+
+    district_tags = bsobj.find('div',{'data-role':'ershoufang'}).find_all('a')
+    #print(district_tags)
+    for tag in district_tags:
+        district_url = tag.get('href')
+        district_name = tag.get_text()
+        district_dict[district_name] = district_url
+    print(district_dict)
+    return district_dict
 
 
-#增加交互
-input_city_name = input('\nplease input the city you want to search:')
+def run():
+    city_dict = get_city_dict()
+    #print(city_dict)
 
-city_url = city_dict.get(input_city_name)
-if city_url:
-    print(input_city_name,city_url)
-else:
-    print('Error!')
-    sys.exit()
+    for city_name in city_dict.keys():
+        print(city_name,end=' ') #如何不换行
+
+    #增加交互
+    input_city_name = input('\nplease input the city you want to search:')
+
+    city_url = city_dict.get(input_city_name)
+    if city_url:
+        print(input_city_name,city_url)
+    else:
+        print('Error!')
+        sys.exit()
+
+    ershoufang_city_url = city_url + 'ershoufang'
+    district_dict = get_district_dict(ershoufang_city_url)
+    for district_name in district_dict.keys():
+        print(district_name,end=' ') #如何不换行
+
+    # 增加交互
+    input_district_name = input('\nplease input the district:')
+
+    district_url = district_dict.get(input_district_name)
+    if district_url:
+        print(input_district_name, district_url)
+    else:
+        print('Error!')
+        sys.exit()
+
+    house_info_url = city_url+district_url[1:]
+    print(house_info_url)
+
+if __name__ == '__main__':
+    run()
+
